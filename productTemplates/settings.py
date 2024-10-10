7340940652c51f1cfd5b118f7a8661dda3168eb4
337Theerakan <theerakankhankham.tk@gmail.com>
2024-10-10 15:23:56 +0700

    first commit

--- productTemplates/settings.py
+++ productTemplates/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'productTemplates.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products255',         
+        'USER': 'rei',                 
+        'PASSWORD': 'theerakan0.0', 
+        'HOST': 'localhost',                    
+        'PORT': '3306',                          
     }
 }
+
 
 
 # Password validation
